aa6585018e2c092cd701e2689d9c9372332fad94
Colton Arthur Allen <allen.colton@hotmail.com>
2026-09-19 10:44:42 -0700

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import { paraglideVitePlugin } from '@inlang/paraglide-js';
+import { mdsvex } from 'mdsvex';
+import adapter from '@sveltejs/adapter-vercel';
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+	plugins: [
+		sveltekit({
+			compilerOptions: {
+				// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
+				runes: ({ filename }) =>
+					filename.split(/[/\\]/).includes('node_modules') ? undefined : true,
+				experimental: { async: true }
+			},
+			adapter: adapter(),
+			preprocess: [mdsvex({ extensions: ['.svx', '.md'] })],
+			extensions: ['.svelte', '.svx', '.md'],
+			experimental: { remoteFunctions: true }
+		}),
+
+		paraglideVitePlugin({
+			project: './project.inlang',
+			outdir: './src/lib/paraglide',
+			emitTsDeclarations: true
+		})
+	]
+});
