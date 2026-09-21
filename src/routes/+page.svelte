18782a413c3ce1759b69b2d5d62febdca68c0fe3
Colton Arthur Allen <allen.colton@hotmail.com>
2026-09-20 17:01:19 -0700

    allow unauthenticated users to see landing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,8 +5,13 @@
 	let { data }: { data: PageServerData } = $props();
 </script>
 
-<h1>Hi, {data.user.name}!</h1>
-<p>Your user ID is {data.user.id}.</p>
-<form method="post" action="?/signOut" use:enhance>
-	<button>Sign out</button>
-</form>
+{#if data.user}
+	<h1>Hi, {data.user.name}!</h1>
+	<p>Your user ID is {data.user.id}.</p>
+	<form method="post" action="?/signOut" use:enhance>
+		<button>Sign out</button>
+	</form>
+{:else}
+	<h1>Welcome!</h1>
+	<p>Please <a href="/login">sign in</a> to continue.</p>
+{/if}
