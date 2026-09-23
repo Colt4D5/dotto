6bdfb2e8bfffdb24577ab7fbd7e50359eafdac95
Colton Arthur Allen <allen.colton@hotmail.com>
2026-09-23 13:22:39 -0700

    add new basic projects page

---
+++ src/routes/dashboard/projects/new/+page.svelte
@@ -0,0 +1,7 @@
+<h1>New Project</h1>
+
+<form>
+  <label for="name">Project Name:</label>
+  <input type="text" id="name" name="name" required>
+  <button type="submit">Create Project</button>
+</form>
